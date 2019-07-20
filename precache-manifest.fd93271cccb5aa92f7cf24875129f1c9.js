b39f83caa0bdafdffb3d376cc4da2ef8c5de8176
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-20 20:04:35 +0000

    Deploy nitingupta220/travis-ci-demo to github.com/nitingupta220/travis-ci-demo.git:gh-pages

---
+++ precache-manifest.fd93271cccb5aa92f7cf24875129f1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f675703e68b3d8f7c6b22af64eeef22e",
+    "url": "/travis-ci-demo/index.html"
+  },
+  {
+    "revision": "5481f4647eb3b7d0f259",
+    "url": "/travis-ci-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/travis-ci-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5481f4647eb3b7d0f259",
+    "url": "/travis-ci-demo/static/js/main.7c2c739f.chunk.js"
+  },
+  {
+    "revision": "c1959699160ade41d9bf",
+    "url": "/travis-ci-demo/static/js/runtime~main.6b559f4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-ci-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
