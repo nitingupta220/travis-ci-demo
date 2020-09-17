56f805a943f370ebf564ca0ba6d2420980da93b4
Nitin Gupta <nitingupta@MacBook-Pro.local>
2020-09-17 17:16:06 +0530

    Updates

--- precache-manifest.fb15ad93a199770b1c3c6ad26bd265b5.js
+++ precache-manifest.fb15ad93a199770b1c3c6ad26bd265b5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a15113ed7c7db13270591acad56b3fab",
+    "revision": "f3ed3a44e224d585459775bc130efa65",
     "url": "/travis-ci-demo/index.html"
   },
   {
-    "revision": "6063b23152049cac929f",
+    "revision": "7e702e33b534809de141",
     "url": "/travis-ci-demo/static/css/main.692d75bb.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/travis-ci-demo/static/js/2.fdc7f0fe.chunk.js"
   },
   {
-    "revision": "6063b23152049cac929f",
-    "url": "/travis-ci-demo/static/js/main.1230a40d.chunk.js"
+    "revision": "7e702e33b534809de141",
+    "url": "/travis-ci-demo/static/js/main.73fbb713.chunk.js"
   },
   {
     "revision": "c1959699160ade41d9bf",
